6e2c70d33ee38ff015bc45397e841a3458fd437f
alexhrodz <alex.hrodz@gmail.com>
2022-07-22 01:04:16 +0300

    first commit

---
+++ wpsync-webspark.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://github.com/alexhrodz/
+ * @since             1.0.0
+ * @package           Wpsync_Webspark
+ *
+ * @wordpress-plugin
+ * Plugin Name:       wpsync-webspark
+ * Plugin URI:        https://github.com/alexhrodz/wpsync-webspark
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            AlexH
+ * Author URI:        https://github.com/alexhrodz/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wpsync-webspark
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'WPSYNC_WEBSPARK_VERSION', '1.0.0' );
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-wpsync-webspark-activator.php
+ */
+function activate_wpsync_webspark() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wpsync-webspark-activator.php';
+	Wpsync_Webspark_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-wpsync-webspark-deactivator.php
+ */
+function deactivate_wpsync_webspark() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wpsync-webspark-deactivator.php';
+	Wpsync_Webspark_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_wpsync_webspark' );
+register_deactivation_hook( __FILE__, 'deactivate_wpsync_webspark' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-wpsync-webspark.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_wpsync_webspark() {
+
+	$plugin = new Wpsync_Webspark();
+	$plugin->run();
+
+}
+run_wpsync_webspark();
+
+defined('ABSPATH') or die('Nope, not accessing this');
+
+
+if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
+    require 'importer.php';
+}
